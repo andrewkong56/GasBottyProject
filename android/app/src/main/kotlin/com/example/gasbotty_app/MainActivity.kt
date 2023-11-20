f17c280270625c2153f4482dc801ca35e9a4683c
andrewkong56 <andrewkong56@hotmail.com>
2023-11-19 22:42:20 -0500

    added GasBotty_App

---
+++ android/app/src/main/kotlin/com/example/gasbotty_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gasbotty_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GasBotty',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('GasBotty')),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 50),
+                Container(
+                  height: 150, 
+                  width: 150,
+                  color: Colors.grey,
+                  child: Center(child: Text("Logo", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold))),
+                ),
+                SizedBox(height: 10),
+                Text("GasBotty", style: TextStyle(fontSize: 20)),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    minimumSize: Size(200, 50),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => CameraScreen()));
+                  },
+                  child: Text('Camera'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    minimumSize: Size(200, 50),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => PriceMatchScreen()));
+                  },
+                  child: Text('Price Match'),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 10.0, 
+            left: 10.0,
+            child: Container(
+              height: 50.0,
+              width: 50.0,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(12),
+                ),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SettingScreen()));
+                },
+                child: Icon(Icons.settings, size: 24.0),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SettingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings')),
+      body: ListView(
+        children: [
+          SwitchListTile(
+            title: const Text('Notifications'),
+            value: false, 
+            onChanged: (bool value) {
+              // what button does when changed
+            },
+          ),
+          SwitchListTile(
+            title: const Text('Appearance'),
+            value: false, 
+            onChanged: (bool value) {
+              // what button does when changed
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PriceMatchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Price match')),
+      body: Center(child: Text('something')),
+    );
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController? _controller;
+  List<CameraDescription>? _cameras;
+  String? imagePath;
+
+  @override
+  void initState() {
+    super.initState();
+    _initCamera();
+  }
+
+  Future<void> _initCamera() async {
+    _cameras = await availableCameras();
+    if (_cameras!.isNotEmpty) {
+      _controller = CameraController(
+        _cameras!.first,
+        ResolutionPreset.high,
+      );
+
+      await _controller!.initialize();
+      if (!mounted) return;
+      setState(() {});
+    }
+  }
+
+  Future<void> _takePicture() async {
+    if (!_controller!.value.isInitialized) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: select a camera first.')),
+      );
+      return;
+    }
+    if (_controller!.value.isTakingPicture) {
+      return;
+    }
+    try {
+      final XFile? file = await _controller!.takePicture();
+      if (file != null) {
+        imagePath = file.path;
+        GallerySaver.saveImage(imagePath!).then((bool? success) {
+          setState(() {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Picture saved to gallery')),
+            );
+          });
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_controller == null || !_controller!.value.isInitialized) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(title: const Text('Camera')),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: CameraPreview(_controller!),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton.icon(
+              icon: const Icon(Icons.camera),
+              label: const Text('Take Picture'),
+              onPressed: _takePicture,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+}
